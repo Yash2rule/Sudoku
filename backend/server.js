49e3a62739a7a3d01c4fcda49a58c518edf75dc6
Yash <yashpvyas19@gmail.com>
2021-07-10 01:10:43 +0530

    Prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -12,8 +12,12 @@ app.use(require('cors')())
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static(path.join(__dirname,'../frontend/build')));
-    app.get('/',(req,res) => {
+    app.get('*',(req,res) => {
         res.sendFile(path.join(__dirname,'../frontend/build/index.html'));
+    })
+}else{
+    app.get('/',(req,res) => {
+        res.send('App is running...');
     })
 }
 
