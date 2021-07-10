7f505ac2788350c56ccfc5aa9c7aae4327da40ba
Yash <yashpvyas19@gmail.com>
2021-07-10 15:26:17 +0530

    Prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -59,5 +59,5 @@ if(process.env.NODE_ENV === 'production'){
     })
 }
 
-const PORT = 5000 || process.env.PORT;
+const PORT = process.env.PORT || 5000;
 server.listen(PORT,() => console.log(`Server running on port ${PORT}`));
