87047c8c3f50f387a9d0d9aebf3ab2bfabc9be52
Yash <yashpvyas19@gmail.com>
2021-07-10 15:11:01 +0530

    Prepare for Deployment

--- backend/server.js
+++ backend/server.js
@@ -53,9 +53,9 @@ io.on('connection',(socket) => {
 })
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static(path.join(__dirname,'/frontend/build')));
+    app.use(express.static(path.join(__dirname,'../frontend/build')));
     app.get('*',(req,res) => {
-        res.sendFile(path.resolve(__dirname,'frontend','build','index.html'))
+        res.sendFile(path.join(__dirname,'../frontend/build/index.html'))
     })
 }
 
